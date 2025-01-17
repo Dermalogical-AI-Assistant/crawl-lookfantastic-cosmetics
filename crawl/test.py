da0efea20a45fc8aacc00a2ae9b8c06785d535ae
jasmineChamCham <jasminebkdn@gmail.com>
2025-01-17 14:49:58 +0700

    feat: crawl product details

--- crawl/test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-product_list = pd.read_csv('./data/product_list.csv', names = ['img', 'title', 'price', 'url', 'skincare_concern'])
-product_list = product_list.drop_duplicates()
-all_urls = list(product_list['url'].unique())
-print(len(all_urls))
